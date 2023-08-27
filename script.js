21861c65d478db01c9e2168ba029267777e6632f
Kevin Martinez <mkevinez@gmail.com>
2023-08-27 13:58:24 -0700

    created function to check for a vertical win when a turn is taken. Tested as well.

--- script.js
+++ script.js
@@ -5,34 +5,56 @@ gameBoard = (() => {
 })();
 
 game = (() => {
-    const playMarker = (marker, square) => {
-        gameBoard.markerArray[square] = marker;
-        return 0;
+
+    const playMarkerLog = () => {
+        return markerPlayed + squarePlayed;
     }
 
-    const checkForWin = (player) => {
-        (checkVerticals(player.marker) || checkHorizontals(player.marker) || checkDiagonals(player.marker)) ? true : false
+    const gameFinished = false;
+    // const activePlayer;
+
+    const playMarker = (markerPlayed, squarePlayed) => {
+        gameBoard.markerArray[squarePlayed] = markerPlayed;
+        return checkForWin(markerPlayed, squarePlayed);
     }
 
-    function checkVerticals(marker) {
+    const checkForWin = (markerPlayed, squarePlayed) => {
+        return (checkVerticals(markerPlayed, squarePlayed) || checkHorizontals(markerPlayed, squarePlayed) || checkDiagonals(markerPlayed, squarePlayed))
+    }
 
+    const checkVerticals = (markerPlayed, squarePlayed) => {
+
+        console.log(squarePlayed);
+        // for each array, subtract the index from marker (0-2) and remainder 3, if the answer is 0, then it is the correct index. Finding two matches. 
+        let counter = 0;
+
+        gameBoard.markerArray.forEach((squareMarker, squareIndex) => {
+            if (markerPlayed === squareMarker) {
+                if ((squarePlayed - squareIndex) % 3 === 0) {
+                    counter++;
+                }
+            }
+        })
+
+        return counter === 3;
         // take the marker, and check the following: matching marker at +3/+6 on index, matching marker at +3/-3 on index, matching marker at -3/-6 on index
 
     }
 
     function checkHorizontals(marker) {
-
+        return false;
         // take the marker, and check the following: matching marker at +1/-1 if index is 1/4/7, matching marker at +1/+2 if index is 0/3/6, matching marker at -1/-2 if index is 2/5/8
 
     }
 
     function checkDiagonals(marker) {
+        return false;
         // take the marker, and check the following: matching marker at +4/+8 if index is 0, matching marker at +2/+4 if index is 2, matching marker at -2/+2 if index is 4
         // matching marker at -2/-4 if index is 6, matching marker at -4/-8 if index is 8
     }
 
 
-    return {playMarker, checkForWin};
+    return {playMarker, checkForWin, checkVerticals};
 })();
 
 function player(name, marker) {
@@ -45,16 +67,16 @@ function player(name, marker) {
 const player1 = player('Kevin', 'X');
 const player2 = player('Alex', 'O');
 
-console.log(player1.marker);
 
-// 
 
-//
+//turn logic: user clicks empty square (disable squares that are not filled), square is updated with marker, check for win or full board, if no win, next turn.
 
-console.log(gameBoard.markerArray);
-
-squareSelected = 7;
-
-game.playMarker(player1.marker, squareSelected);
-
+console.log(game.playMarker(player1.marker, 0));
+console.log(game.playMarker(player2.marker, 2));
+console.log(game.playMarker(player1.marker, 4));
+console.log(game.playMarker(player2.marker, 3));
+console.log(game.playMarker(player1.marker, 6));
+console.log(game.playMarker(player2.marker, 8));
+console.log(game.playMarker(player1.marker, 7));
+console.log(game.playMarker(player2.marker, 1));
 console.log(gameBoard.markerArray);
